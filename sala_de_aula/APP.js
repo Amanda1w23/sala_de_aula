4453297692dc7437e0534e6aea48ae646777becb
amanda cristiny kono chein <amanda.chemin@escola.pr.gov.br>
2024-06-07 12:04:32 -0300

    inserindo os primeiros passos

---
+++ sala_de_aula/APP.js
@@ -0,0 +1,12 @@
+let priNum = prompt ('Digite um número:');
+let segNUm = prompt ('Digite outro número:');
+priNum = parseFloat (priNum);
+segNUm = parseFloat (segNUm);
+let soma = priNum + segNUm
+let sub =priNum - segNUm;
+
+alert('Somarei os valores informados...');
+alert('a soma de '+ priNum +' e '+ segNUm +' é '+ sub)
+
+alert('Agora  vou subtai-los...')
+alert('a subtragão entre '+ priNum +' é '+ sub)
